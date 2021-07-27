9673c35528e3dde6fd21d627ebb353955dc98500
kalyane <kalyane.bezerra02@gmail.com>
2021-07-27 11:40:47 -0700

    Fixing Bugs Kalyane

--- ment2meet/src/App.js
+++ ment2meet/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Router, Route, hashHistory } from 'react-router';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home'
 import MasterForm  from './form/MasterForm';
+import Card  from './groupSessionCards/Card';
 
 
 function App() {
